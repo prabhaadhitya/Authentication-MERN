8a1383c5afeab2c10121980f133f94f5d62b33f6
Prabha Adhitya <prabhaadhitya01@gmail.com>
2025-08-27 02:24:27 +0530

    Routes Created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config"
 import cookieParser from "cookie-parser";
 import connectDb from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js"
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,9 +13,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}))
 
-app.get('/', (req, res) => {
-    res.send("API working!!!")
-})
+// API Endpoints
+app.get('/', (req, res) => res.send("API working!!!"));
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => {
     console.log(`Server started on PORT: ${port}`);
