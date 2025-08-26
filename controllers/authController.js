d2d31a04783031d3439736c5210ffec0a7c60e3d
Prabha Adhitya <prabhaadhitya01@gmail.com>
2025-08-27 01:54:23 +0530

    Login API Created

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,35 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
+        return res.json({success: true});
     } catch (error) {
         res.json({success: false, message: error.message})
+    }
+}
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return res.json({success: false, message: "Email and Password are required"})
+    }
+    try {
+        const user = await userModel.findOne({email})
+        if (!user) {
+            return res.json({success: false, message: "Invalid email"})
+        }
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            return res.json({success: false, message: "Invalid password"})
+        }
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+        return res.json({success: true});
+    } catch (error) {
+        return res.json({success: false, message: error.message})
     }
 }
